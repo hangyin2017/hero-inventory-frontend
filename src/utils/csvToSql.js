c3e353eef420476d2aac3e728d1d976a5edcbe2e
Hang <hang.yin2017@outlook.com>
2020-11-12 18:36:01 +1100

    Created new fields for data.

--- src/utils/csvToSql.js
+++ src/utils/csvToSql.js
@@ -1,17 +1,23 @@
 'use strict'
 
 const fs = require('fs');
-const START_COLUMN = 1;
-const fileDirectory = '../../docs/resource/';
-const fileName = 'brands';
+const path = require('path');
+const START_COLUMN = 0;
+const TARGET_DIRECTORY = '../../docs/resources/';
+const FILE_NAME = 'items3';
 
 function rowToSqlQuery(row, startColumn) {
   let query = '(';
 
-  const cells = row.split(',');
+  const cells = row.trim().split(',');
 
   for (let i = startColumn; i < cells.length; i++) {
-    query += `'${cells[i]}'`;
+    if (cells[i].length === 0) {
+      query += 'null';
+    } else {
+      query += `'${stringSanitiser(cells[i])}'`;
+    }
+    
     if (i !== cells.length - 1)
       query += ',';
   }
@@ -20,13 +26,18 @@ function rowToSqlQuery(row, startColumn) {
   return query;
 }
 
-const inputUrl = fileDirectory + fileName + '.csv';
-const outputUrl = fileDirectory + fileName + '.sql';
+function stringSanitiser(string) {
+  return string.replace('"', '').replace('"', '').replace('\'', '\'\'').trim();
+}
 
-fs.readFile(inputUrl, 'utf8', (err, data) => {
-  const rows = data.trim().split('\n');
+const inputUrl = path.join(__dirname, TARGET_DIRECTORY, FILE_NAME + '.csv');
+const outputUrl = path.join(__dirname, TARGET_DIRECTORY, FILE_NAME + '.sql');
 
-  const sqlQuerys = rows.map((row) => rowToSqlQuery(row, START_COLUMN));
+const data = fs.readFileSync(inputUrl, 'utf8').trim();
 
-  fs.writeFileSync(outputUrl, sqlQuerys.join(',\n'));
-});
+const sqlQuerys = data
+    .split('\n')
+    .map((row) => rowToSqlQuery(row, START_COLUMN))
+    .join(',\n');
+
+fs.writeFileSync(outputUrl, sqlQuerys, 'utf8');
