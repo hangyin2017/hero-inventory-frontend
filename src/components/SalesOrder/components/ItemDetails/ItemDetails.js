0ee5fdecc91026a00869d30e76d74626683bdba2
Dennis0804 <dennissally1314@gmail.com>
2020-11-19 12:02:35 +1100

    Add OrderedItemTable

---
+++ src/components/SalesOrder/components/ItemDetails/ItemDetails.js
@@ -0,0 +1,10 @@
+import React from "react";
+import OrderedItemTable from "./components/OrderedItemTable/OrderedItemTable"
+
+const ItemDetails = () => {
+  return (
+    <OrderedItemTable />
+  )
+}
+
+export default ItemDetails;
