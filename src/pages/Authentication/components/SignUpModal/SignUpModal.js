c122824e6270d28c39643675c8b58f9769407352
Hang <hang.yin2017@outlook.com>
2020-11-27 16:58:09 +1100

    Moved FormItem

--- src/pages/Authentication/components/SignUpModal/SignUpModal.js
+++ src/pages/Authentication/components/SignUpModal/SignUpModal.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import Modal from "../Modal";
-import FormItem from "../../../../components/FormItem";
+import FormItem from "../FormItem";
 
 const Form = styled.form`
   padding: 16px 0;
