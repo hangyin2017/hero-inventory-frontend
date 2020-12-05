c8b70a66f47d8ba05f9245ff155d424963499888
finnick-666 <finnick9387@gmail.com>
2020-12-05 16:34:43 +1100

    Create fields

---
+++ src/pages/Customers/components/NewCustomerModal/fields.js
@@ -0,0 +1,56 @@
+export default {
+	customer_name: {
+		label: 'Customer Name',
+		editable: true,
+		required: true,
+		errorMessage: 'Customer Name is required',
+	},
+	company_name: {
+		label: 'Company Name',
+		editable: true,
+	},
+	website: {
+		label: 'Website',
+		editable: true,
+	},
+	contact_type: {
+		label: 'Contact Type',
+		editable:true,
+	},
+	active: {
+		label: 'Active',
+		editable: false,
+	},
+	salutation: {
+		label: 'Salutation',
+		editable:true,
+	},
+	first_name: {
+		label: 'First Name',
+		editable: true,
+	},
+	last_name: {
+		label: 'Last Name',
+		editable: true,
+	},
+	email: {
+		label: 'Email',
+		editable:true,
+	},
+	phone: {
+		label: 'Phone',
+		editable:true,
+	},
+	created_time: {
+		label: 'Created Time',
+		editable: false,
+	},
+	last_modified_time: {
+		label: 'Last Modified Time',
+		editable: false,
+	},
+	comments: {
+		label: 'Comments',
+		editable:true,
+	}
+};
