de8383e51f1c57fe4a57c1cdd30bac455a43e3fc
finnick-666 <finnick9387@gmail.com>
2020-12-21 21:22:11 +1100

    finish product details(ittem counts)

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -3,19 +3,33 @@ import React from "react";
 import Page from '../../components/Page';
 import dashboard from '../../apis/dashboard';
 import ItemCounts from "./components/ItemCounts";
+import PAGES from '../../pages';
+import { Col, Row } from "antd";
 
 
-export const Dashboard = () => {
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <Page
-      headerProps={{
-        title: 'Dashboard',
-      }}
-      api={dashboard}
-    ><ItemCounts></ItemCounts>
-    </Page>
-  );
-};
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <Page
+        headerProps={{
+          title: PAGES.dashboard.title,
+        }}
+        api={dashboard}
+      ><Row>
+        <Col span={10}>
+          <ItemCounts />
+        </Col>
+      </Row>
+      </Page>
+    );
+  };
+}
 
 export default Dashboard;
--- src/pages/Dashboard/components/ItemCounts/ItemCounts.js
+++ src/pages/Dashboard/components/ItemCounts/ItemCounts.js
@@ -1,30 +1,55 @@
-import { Card, Col, Row, Statistic } from "antd";
+import { Card, Col, Row, Statistic, message } from "antd";
 import React from "react";
+import dashboard from "../../../../apis/dashboard";
+import withFetch from '../../../../components/withFetch';
 
 
-
-export const ItemCounts = () => {
-
-  const cardStyle = {
-    textAlign: 'center',
-  };
-
-  return (
-    <Row>
-      <Col span={12}>
-        <Card title="Product Details" style={cardStyle} hoverable={true}>
-            <Row>
-              <Col span={12}>
-                <Statistic title="All Items" value={123} />
-                </Col>
-              <Col span={12}>
-                <Statistic title="Low Stock Items" value={123} />
-              </Col>
-            </Row>
-        </Card>
-      </Col>
-    </Row>
-  );
+const cardStyle = {
+  textAlign: 'center',
+  margin: 10
 };
+class ItemCounts extends React.Component {
 
-export default ItemCounts;
+  constructor(props) {
+    super(props);
+
+    this.state={
+      data:{},
+    }
+
+  }
+
+  async componentDidMount() {
+    this.refreshData();
+  }
+
+  async refreshData() {
+    const { fetch } = this.props;
+
+    try {
+      const data = await fetch(() => dashboard.getAll());
+      this.setState({ data });
+    } catch(err) {
+      message.error(`Something went wrong while fetching details`);
+    }
+  }
+
+
+  render() {
+    return (
+          <Card title="Product Details" style={cardStyle} hoverable={true}>
+              <Row>
+                <Col span={12}>
+                  <Statistic title="All Items" value={this.state.data.itemCount} />
+                  </Col>
+                <Col span={12}>
+                  <Statistic title="Low Stock Items" value={this.state.data.lowStockItemCount}  valueStyle={{ color: '#cf1322' }} />
+                </Col>
+              </Row>
+          </Card>
+    );
+  };
+  }
+  
+
+export default withFetch(ItemCounts);
