f445e2cf8557fb6be1fa8f8f442a67bea8c432a5
finnick-666 <finnick9387@gmail.com>
2020-12-23 19:13:21 +1100

    add dashboard footer

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -5,10 +5,18 @@ import dashboard from '../../apis/dashboard';
 import ItemCounts from "./components/ItemCounts";
 import DashboardCalendar from "./components/DashboardCalendar";
 import PAGES from '../../pages';
-import { Col, Row } from "antd";
+import { Col, Row, Divider } from "antd";
+import styled from 'styled-components';
 import OrderCounts from "./components/OrderCounts";
 
-
+const StyledCol = styled(Col)`
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+`
+const Footer = styled.div`
+text-align:center;
+`
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
@@ -24,15 +32,18 @@ class Dashboard extends React.Component {
           title: PAGES.dashboard.title,
         }}
         api={dashboard}
-      ><Row>
-        <Col span={8}>
+      >
+        <Row>
+        <StyledCol span={8}>
           <ItemCounts />
           <OrderCounts />
-        </Col>
+        </StyledCol>
         <Col span={16}>
           <DashboardCalendar />
         </Col>
       </Row>
+      <Divider />
+      <Footer> © 2020, Hero Inventory Group. All Rights Reserved. </Footer>
       </Page>
     );
   };
