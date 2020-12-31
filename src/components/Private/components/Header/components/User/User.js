8da9873b0870dc692887d27a21d9cf7e7f8bb2c0
Hang <hang.yin2017@outlook.com>
2021-01-01 00:42:46 +1100

    User profile overlay

--- src/components/Private/components/Header/components/User/User.js
+++ src/components/Private/components/Header/components/User/User.js
@@ -7,7 +7,17 @@ import { UserOutlined } from '@ant-design/icons';
 import { color } from '../../../../../../styles';
 
 const Wrapper = styled.div`
+`;
 
+const Overlay = styled.div`
+  position: fixed;
+  z-index: -1;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
 `;
 
 const Profile = styled.div`
@@ -60,10 +70,14 @@ const User = ({ authentication }) => {
   const { username = 'user', id = 'unknown', email = 'email' } = user;
   return (
     <Wrapper>
-      <div onClick={() => setShow(!showProfile)} >
+      <div onClick={(e) => {
+        e.stopPropagation();
+        setShow(!showProfile);
+      }}>
         <Avatar style={{ backgroundColor: "lightblue" }} icon={<UserOutlined />} />
       </div>
       <Profile visible={showProfile}>
+        {showProfile && <Overlay onClick={() => setShow(false)} />}
         <AvatarWrapper>
           <Avatar size={64} icon={<UserOutlined />} />
         </AvatarWrapper>
