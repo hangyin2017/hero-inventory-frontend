dc0339e19a2a83a69e2c466e03e3b31e6968ce12
Hang <hang.yin2017@outlook.com>
2020-12-31 15:03:20 +1100

    Header logo

--- src/components/Private/components/Header/Header.js
+++ src/components/Private/components/Header/Header.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import Authentication from './components/Authentication';
+import { Anchor, Avatar } from 'antd';
+import logo from '../../../../assets/img/logo.png';
+import { UserOutlined } from '@ant-design/icons';
+import { HOMEPAGE } from '../../../../Routes';
 
 const Wrapper = styled.div`
   display: flex;
@@ -10,10 +13,13 @@ const Wrapper = styled.div`
   text-align: center;
 `;
 
-const Logo = styled.div`
+const StyledLogo = styled.div`
   flex: 0 0 200px;
-  padding: 15px 15px;
   border-right: 1px solid #eee;
+
+  & img {
+    height: 55px;
+  }
 `;
 
 const Main = styled.div`
@@ -32,34 +38,71 @@ const Right = styled.div`
   align-items: center;
 `;
 
-const Avatar = styled.div`
-  display: flex;
-  align-items: center;
-  width: 35px;
-  height: 35px;
-  border-radius: 50%;
-  background-color: blue;
-  color: #fff;
+const Dropdown = styled.div`
+  width: 360px;
+  height: 240px;
+  position: absolute;
+  z-index: 3;
+  right: 0;
+  padding: 10px 20px;
+  background-color: #eefaff;
 `;
 
-const Header = () => {
-  return (
-    <Wrapper>
-      <Logo>
-        Logo
-      </Logo>
-      <Main>
-        <Left>
-        </Left>
-        <Right>
-          <Authentication />
-          <Avatar>
-            A
-          </Avatar>
-        </Right>
-      </Main>
-    </Wrapper>
-  );
-};
+const Logout = styled.div`
+  color: red;
+`;
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      show: false,
+    };
+  }
+
+  render() {
+    const { authentication } = this.props;
+    const { show } = this.state;
+
+    return (
+      <Wrapper>
+        <StyledLogo>
+          <a href={HOMEPAGE}>
+            <img src={logo} alt="logo" />
+          </a>
+        </StyledLogo>
+        <Main>
+          <Left></Left>
+          <Right>
+            {/* <Authentication /> */}
+            <div>
+              <div
+                onClick={() => {
+                  this.setState({ show: !show });
+                }}
+              >
+                <Avatar style={{ backgroundColor: "lightblue" }} icon={<UserOutlined />} />
+              </div>
+              {this.state.show ? (
+                <Dropdown>
+                  <h3>
+                    <Avatar size={64} icon={<UserOutlined />} />
+                  </h3>
+                  <div class="">zixiangli0516</div>
+                  <div class="">User ID : 123456</div>
+                  <p>
+                    <small class="">zixiangli0516@gmail.com</small>
+                  </p>
+                  <Logout onClick>Log out</Logout>
+                </Dropdown>
+              ) : null}
+            </div>
+          </Right>
+        </Main>
+      </Wrapper>
+    );
+  }
+}
 
 export default Header;
