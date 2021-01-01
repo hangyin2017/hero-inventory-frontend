9682d433c8fedda73a78f7a4068d402d5e50f834
Hang <hang.yin2017@outlook.com>
2021-01-01 20:41:26 +1100

    Website icon

--- src/apis/signIn/signIn.js
+++
@@ -1,7 +0,0 @@
-import instance from '../../lib/instance';
-
-const signIn = (data) => instance
-  .post('authentication/sign-in', data)
-  .then((response) => response.data);
-
-  export default signIn;
--- src/apis/signUp/signUp.js
+++
@@ -1,9 +0,0 @@
-import instance from '../../lib/instance';
-
-const URL = '/api/v1/salesorder'; 
-
-const signUp = (data) => instance
-  .post('/api/v1/auth/sign-up', data)
-  .then((response) => response.data);
-
-  export default signUp;
