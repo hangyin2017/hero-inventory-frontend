9539e2c451bfcbb485ea080dc4b29692930993d5
Hang <hang.yin2017@outlook.com>
2021-01-04 20:41:17 +1100

    Loading image

--- src/pages/Inventory/components/ItemDetailModal/components/ItemImage/ItemImage.js
+++ src/pages/Inventory/components/ItemDetailModal/components/ItemImage/ItemImage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Image, Upload } from 'antd';
 import { InboxOutlined } from '@ant-design/icons';
-import { baseUrl } from '../../../../../../lib/aws';
+import s3, { baseUrl } from '../../../../../../lib/aws';
 
 const { Dragger } = Upload;
 
@@ -18,26 +18,64 @@ class ItemImage extends React.Component {
     super(props);
 
     this.state = {
+      loading: true,
       hasImage: false,
     };
   }
 
+  componentDidMount() {
+    this.getImage();
+  }
+
+  getImage() {
+    const { id } = this.props;
+
+    this.setState({ loading: true });
+
+    s3.listObjects({Prefix: `${id}/`}, (err, data) => {
+      this.setState({ loading: false });
+
+      if(err) {
+        return;
+      }
+
+      console.log(data);
+      const images = data.Contents.filter((obj) => obj.Size > 0);
+
+      if(images.length === 0 ) {
+        return;
+      }
+
+      this.setState({ hasImage: true });
+    })
+  }
+
   render() {
     const { id } = this.props;
+    const { loading, hasImage } = this.state;
     const photoUrl = `${baseUrl}${id}/1.jpg`;
+
+    if(loading) {
+      return (
+        <Wrapper></Wrapper>
+      );
+    }
 
     return (
       <Wrapper>
-        {/* <Image height={200} src={photoUrl} alt="item image" /> */}
-        <Dragger>
-          <p className="ant-upload-drag-icon">
-            <InboxOutlined />
-          </p>
-          <p className="ant-upload-text">Click or drag file to this area to upload</p>
-          <p className="ant-upload-hint">
-            You can add up to 15 images, each not exceeding 5 MB.
-          </p>
-        </Dragger>
+        {hasImage ? (
+          <Image height={200} src={photoUrl} alt="item image" />
+        ) : (
+          <Dragger disabled={loading}>
+            <p className="ant-upload-drag-icon">
+              <InboxOutlined />
+            </p>
+            <p className="ant-upload-text">Click or drag file to this area to upload</p>
+            <p className="ant-upload-hint">
+              You can add up to 15 images, each not exceeding 5 MB.
+            </p>
+          </Dragger>
+        )}
       </Wrapper>
     );
   }
