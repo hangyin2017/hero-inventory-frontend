fd7cd222007df3e622610c425d2d3e62b9874ee4
Hang <hang.yin2017@outlook.com>
2021-02-02 20:44:36 +1100

    Lazy load test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Guard from './components/Guard';
-import Private from './components/Private';
-import { AuthenticationProvider } from './components/withAuthentication';
-import { AUTH_ROUTE } from './Routes';
+import React, { Suspense } from 'react';
+import Lazy from './Lazy';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import Guard from './components/Guard';
+// import Private from './components/Private';
+// import { AuthenticationProvider } from './components/withAuthentication';
+// import { AUTH_ROUTE } from './Routes';
+
+// const Lazy = React.lazy(() => import(/* webpackChunkName: "lazy" */ './Lazy'));
 
 const App = () => {
   return (
-    <AuthenticationProvider>
-      <Router>
-        <Switch>
-          <Route path={AUTH_ROUTE.path}>
-            <AUTH_ROUTE.component />
-          </Route>
-          <Route path="/">
-            <Guard>
-              <Private />
-            </Guard>
-          </Route>
-        </Switch>
-      </Router>
-    </AuthenticationProvider>
+    <Suspense fallback={<p>loading</p>}>
+      <Lazy />
+    </Suspense>
+    // <AuthenticationProvider>
+    //   <Router>
+    //     <Switch>
+    //       <Route path={AUTH_ROUTE.path}>
+    //         <AUTH_ROUTE.component />
+    //       </Route>
+    //       <Route path="/">
+    //         <Guard>
+    //           <Private />
+    //         </Guard>
+    //       </Route>
+    //     </Switch>
+    //   </Router>
+    // </AuthenticationProvider>
   );
 };
 
---
+++ src/Lazy.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Select } from 'antd';
+
+const Lazy = () => {
+  return (
+    <div>
+      <h1>Lazy component</h1>
+      <Select />
+    </div>
+  );
+};
+
+export default Lazy;
